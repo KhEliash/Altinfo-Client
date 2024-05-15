8ded0ff29a5226d3ae2c4b555aa759e8cadd97f9
Kh.Eliash <eliashebrahim@gmail.com>
2024-05-15 12:15:23 +0600

    get my recommendation data

--- src/pages/MyRecommendation.jsx
+++ src/pages/MyRecommendation.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+
+ 
 
 const MyRecommendation = () => {
+    const {user}= useContext(AuthContext);
+      // all recom
+  useEffect(() => {
+    fetch(` http://localhost:5000/myrecom?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setQueries(data);
+      });
+  }, [user]);
+
+
     return (
         <div>
-            myre
+            my
         </div>
     );
 };
