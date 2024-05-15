b88eeea4f3a412b8ac42cc5bf32dbb10d62ff2c5
Kh.Eliash <eliashebrahim@gmail.com>
2024-05-15 15:58:03 +0600

    delete and recommendation count decrease

--- src/pages/MyRecommendation.jsx
+++ src/pages/MyRecommendation.jsx
@@ -6,18 +6,19 @@ const MyRecommendation = () => {
   const { user } = useContext(AuthContext);
   const [queries, setQueries] = useState([]);
   const [control, setControl] = useState(false);
+  //   console.log(queries);
   // all recom
   useEffect(() => {
     fetch(` http://localhost:5000/myrecom?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setQueries(data);
       });
-  }, [user,control]);
+  }, [user, control]);
 
-
-  const handleDelete = (id) => {
+  const handleDelete = (id1, id2) => {
+    // console.log(id1, id2);
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -28,18 +29,28 @@ const MyRecommendation = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        fetch(` http://localhost:5000/delete/${id}`, {
+        fetch(` http://localhost:5000/delete/${id1}`, {
           method: "DELETE",
         })
           .then((res) => res.json())
           .then((data) => {
-            console.log(data);
+            // console.log(data);
             if (data.deletedCount > 0) {
               Swal.fire({
                 title: "Deleted!",
                 text: "Your file has been deleted.",
                 icon: "success",
               });
+
+              fetch(`http://localhost:5000/updateInc/${id2}`, {
+                method: "PUT",
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  //   console.log(data);
+                  //   setControl(!control);
+                });
+
               setControl(!control);
             }
           });
@@ -66,30 +77,23 @@ const MyRecommendation = () => {
               </tr>
             </thead>
             <tbody>
-              {queries.map((c,index) => (
-  
-              <tr key={c._id}>
-                <th>
-                  {index + 1}
-                </th>
-                <td>
-                  {c.productName}
-                </td>
-                <td>
-                 {c.userName}
-                </td>
-                <td>
-                    <img src={c.productImage} alt="" className="w-12 h-12"/>
-                </td>
-                <th>
-                  <button
-                   onClick={() => handleDelete(c._id)}
-                  className="btn bg-rose-600 text-white btn-xs">
-                    
-                    Delete
-                  </button>
-                </th>
-              </tr>
+              {queries.map((c, index) => (
+                <tr key={c._id}>
+                  <th>{index + 1}</th>
+                  <td>{c.productName}</td>
+                  <td>{c.userName}</td>
+                  <td>
+                    <img src={c.productImage} alt="" className="w-12 h-12" />
+                  </td>
+                  <th>
+                    <button
+                      onClick={() => handleDelete(c._id, c.queryId)}
+                      className="btn bg-rose-600 text-white btn-xs"
+                    >
+                      Delete
+                    </button>
+                  </th>
+                </tr>
               ))}
             </tbody>
           </table>
