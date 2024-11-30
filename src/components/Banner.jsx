f513f4f3d993ccd367f0359397a5209e9771cd76
KhEliash <eliashebrahim@gmail.com>
2024-11-30 22:22:03 +0600

    change hover color

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
       <div className="bg-gray-200 p-4 text-center">
         <h2 className="text-2xl font-semibold mb-4 text-[#151515]">Explore All Queries</h2>
         <Link to={'/queries'}
-          className="inline-block px-4 py-2 bg-[#A91D3A] text-white rounded hover:bg-blue-600"
+          className="inline-block px-4 py-2 bg-[#A91D3A] text-white rounded hover:bg-rose-600 hover:scale-105 transition duration-100"
         >
           Take Me There
         </Link>
