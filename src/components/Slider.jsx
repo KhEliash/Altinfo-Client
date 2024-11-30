da48f7899ab0fbe791ee92fe5c0905eafc6ce283
KhEliash <eliashebrahim@gmail.com>
2024-11-30 22:05:24 +0600

    changed the banner text color and image position

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,4 @@
-import {
-  A11y,
-  Autoplay,
-  Navigation,
-  Pagination,
- 
-} from "swiper/modules";
+import { A11y, Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -19,35 +13,33 @@ const Slider = () => {
     <div>
       <div className="   relative h-[400px] lg:h-[600px]">
         <div className="flex bg-[#C73659] h-full">
-          <div className=" hidden  w-1/2 lg:flex items-center justify-start pl-5">
+          <div className=" hidden  w-1/2 lg:flex items-center justify-start pl-10">
             <Slide cascade>
-              <h1 className="text-3xl lg:text-5xl font-extrabold text-[#151515]">
+              <h1 className="text-3xl lg:text-5xl font-extrabold text-gray-100 dark:text-[#151515] ">
                 Unlocking Alternative Solutions: Explore, Compare, Decide with
                 AltInfoHub.
               </h1>
             </Slide>
           </div>
-          <div className=" w-full lg:w-1/2 absolute bottom-0 top-0  right-0  ">
+          <div className=" w-full lg:w-1/2 absolute bottom-0 top-0  right-0 ">
             <Swiper
               // install Swiper modules
               modules={[Navigation, Pagination, A11y, Autoplay]}
               spaceBetween={50}
               slidesPerView={1}
-              
               autoplay={{
                 delay: 2500,
                 disableOnInteraction: false,
               }}
               pagination={{ clickable: true }}
-              
             >
               {/* slide 1 */}
-              <SwiperSlide className=" mt-24  my-auto ">
-                <div className=" flex items-center">
+              <SwiperSlide className="  my-auto  ">
+                <div className=" flex items-center justify-center">
                   <img
                     src="https://i.ibb.co/b2JH3W9/Remove-bg-ai-1715400363952.png"
                     alt="img"
-                    className="w-full h-[300px] lg:h-[400px] rounded-xl "
+                    className="  h-[300px] lg:h-[400px] rounded-xl"
                   />
                 </div>
               </SwiperSlide>
@@ -57,17 +49,17 @@ const Slider = () => {
                   <img
                     src=" https://i.ibb.co/t3M5CTT/Remove-bg-ai-1715400409266.png"
                     alt="img"
-                    className="w-full h-[300px] lg:h-[400px] mr-24"
+                    className=" h-[300px] lg:h-[400px] mr-24"
                   />
                 </div>
               </SwiperSlide>
               {/* slide 3 */}
               <SwiperSlide className="   my-auto ">
-                <div className="   ">
+                <div className="  flex items-center justify-center ">
                   <img
                     src=" https://i.ibb.co/bFQftdY/Remove-bg-ai-1715400396244.png"
                     alt="img"
-                    className="w-full h-[300px] lg:h-[500px] rounded-xl mb-24"
+                    className="  h-[300px] lg:h-[500px] rounded-xl mb-24"
                   />
                 </div>
               </SwiperSlide>
