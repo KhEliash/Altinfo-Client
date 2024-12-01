f462995be87222a8afdac713a7367d2805ffaa19
KhEliash <eliashebrahim@gmail.com>
2024-12-01 19:50:06 +0600

    convert into local time

--- src/components/LatestCard.jsx
+++ src/components/LatestCard.jsx
@@ -14,7 +14,7 @@ const LatestCard = () => {
     const firstSix = sortedQueries.slice(0, 6);
     setFirstSixQueries(firstSix);
   }, [queries]);
-  // console.log(queries);
+  console.log(queries);
   return (
     <div className="mt-12 bg-base-200 p-5">
       <div className="text-center text-3xl font-bold my-10 ">
@@ -22,60 +22,6 @@ const LatestCard = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {firstSixQueries.map((c) => (
-          // <div key={c._id} className="card bg-base-100 shadow-xl">
-          //   <figure className="px-10 pt-10">
-          //     <img
-          //       src={c.productImageURL}
-          //       alt="Shoes"
-          //       className="rounded-xl h-[150px] "
-          //     />
-          //   </figure>
-          //   <div className="card-body items-center text-center">
-          //     <h2 className="card-title">
-          //       <span className="font-bold text-2xl text-[#C73659]">Name:</span>
-          //       {c.productName}
-          //     </h2>
-          //     <p className="text-2xl font-bold">
-          //       <span className="font-bold text-2xl text-[#C73659]">
-          //         Company:
-          //       </span>{" "}
-          //       {c.productBrand}
-          //     </p>
-          //     <div className="text-left text-gray-600 space-y-3">
-          //       <p className="text-2xl ">
-          //         <span className="font-bold text-2xl text-[#C73659]">
-          //           Title:{" "}
-          //         </span>
-          //         {c.queryTitle.slice(0, 50)}...
-          //       </p>
-          //       <p className=" text-2xl">
-          //         <span className="font-bold text-2xl text-[#C73659]">
-          //           Reason:{" "}
-          //         </span>
-          //         {c.boycottingReason.slice(0, 90)}...
-          //       </p>
-          //     </div>
-          //     <p className="text-left w-full">
-          //       <span className="font-bold text-2xl text-[#C73659]">
-          //         Posted Time:{" "}
-          //       </span>
-          //       {c.userInfo.currentTimes}
-          //     </p>
-          //     <div className="flex items-center justify-around bg-base-200 p-2 rounded-xl w-full">
-          //       <img
-          //         src={c.userInfo.userImage}
-          //         alt=""
-          //         className="rounded-full"
-          //       />
-          //       <h3 className="text-2xl font-bold ">
-          //         <span className="font-bold text-2xl text-[#C73659]">
-          //           User Name:{" "}
-          //         </span>
-          //         {c.userInfo.userName}
-          //       </h3>
-          //     </div>
-          //   </div>
-          // </div>
           <div
             key={c._id}
             className="card bg-base-100 shadow-md rounded-lg overflow-hidden  "
@@ -114,34 +60,28 @@ const LatestCard = () => {
 
               {/* Posted Time */}
               <p className="mt-2 text-gray-500 text-xs">
-                <span className="font-medium text-[#C73659]">Posted Time:</span>
-                {c.userInfo.currentTimes}
+                <span className="font-medium text-[#C73659]">
+                  Posted Time:{" "}
+                </span>
+                {new Date(c.userInfo.currentTimes).toLocaleString()}
               </p>
             </div>
 
             {/* User Info */}
             <div className="flex items-center bg-base-200 px-4 py-3 border-t border-gray-300">
               <img
-                src={c.userInfo.userImage}
-                alt="User Avatar"
+                src={
+                  c?.userInfo?.userImage || "https://via.placeholder.com/150"
+                }
+                alt="User Image"
                 className="w-10 h-10 rounded-full object-cover mr-3"
               />
               <div>
                 <h3 className="text-sm font-semibold ">
-                  {c.userInfo.userName}
+                  {c?.userInfo?.userName}
                 </h3>
-                {/* <p className="text-xs text-gray-500">
-                  Recommendations: {c.recommendationCount || 0}
-                </p> */}
               </div>
             </div>
-
-            {/* Recommend Button */}
-            {/* <div className="p-4">
-              <button className="w-full px-3 py-2 bg-[#C73659] text-white font-semibold rounded-md hover:bg-[#a52847] transition">
-                Recommend
-              </button>
-            </div> */}
           </div>
         ))}
       </div>
