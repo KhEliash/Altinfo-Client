272826b881bc1ecb0e37cab66b88308de180386f
KhEliash <eliashebrahim@gmail.com>
2024-12-02 22:26:51 +0600

    btn color change

--- src/pages/authentication/Registration.jsx
+++ src/pages/authentication/Registration.jsx
@@ -101,7 +101,7 @@ const Registration = () => {
             </div>
 
             <div className="mt-6">
-              <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-rose-800 rounded-lg hover:bg-rose-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
                 Sign Up
               </button>
             </div>
